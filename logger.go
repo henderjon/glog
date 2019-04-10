e9a04713ecbf0b7054d1343baf994e3074415cb9
henderjon <list@box370.net>
2019-04-10 10:37:25 -0500

    uses colon like the log package

--- logger.go
+++ logger.go
@@ -7,10 +7,10 @@ import (
 
 // Error, Warning, Info, and Debug define prefixes for logged output that signal a level of severity.
 const (
-	Error   = "! error; "   // errors are when both the operation and the application can't proceed
-	Warning = "# warning; " // warnings (alerts) are when the operation cannot proceed but the application can
-	Debug   = ": debug; "   // debug is used for debugging messages that will most likely be silenced/removed in production
-	Info    = ". info; "    // info is used for informational purposes like status messages for long running processed
+	Error   = "! error: "   // errors are when both the operation and the application can't proceed
+	Warning = "# warning: " // warnings (alerts) are when the operation cannot proceed but the application can
+	Debug   = ": debug: "   // debug is used for debugging messages that will most likely be silenced/removed in production
+	Info    = ". info: "    // info is used for informational purposes like status messages for long running processed
 	None    = ""            // None is used when the information is intentional often intended for stdout
 	devnull = 0             // ioutil.Discard
 	stdout  = 1             // os.Stdout
