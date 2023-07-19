799a790c2525b846ad3e02b5eec98c21b93f3aca
henderjon <list@box370.net>
2023-07-19 14:40:44 -0500

    use embedding to postmarkedlogger

--- postmarked_logger.go
+++ postmarked_logger.go
@@ -2,37 +2,32 @@ package logger
 
 // A PostmarkedLogger is a regular instance of Logger but will Postmark all entries.
 type PostmarkedLogger struct {
-	log Logger
+	Logger
 }
 
 // NewPostmarkedLogger creates a new logger that assumes Postmark is true for all entries
 func NewPostmarkedLogger(l Logger) *PostmarkedLogger {
 	return &PostmarkedLogger{
-		log: l,
+		l,
 	}
-}
-
-// Logger returns the underlying Logger
-func (l PostmarkedLogger) Logger() Logger {
-	return l.log
 }
 
 // Log fulfills the Logger interface. It writes the entry to the underlying destination
 func (l PostmarkedLogger) Log(args ...interface{}) {
 	args = append(args, Postmark)
 	e := entry(args...)
-	l.log.Log(e)
+	l.Log(e)
 }
 
 // Fatal fulfills the Logger interface. It writes the entry to the underlying destination then exits
 func (l PostmarkedLogger) Fatal(args ...interface{}) {
 	args = append(args, Postmark)
 	e := entry(args...)
-	l.log.Fatal(e)
+	l.Fatal(e)
 }
 
 // Write fulfills the io.Writer interface
 func (l PostmarkedLogger) Write(p []byte) (n int, err error) {
 	e := entry(p, Postmark)
-	return l.log.Write([]byte(e.String()))
+	return l.Write([]byte(e.String()))
 }
